f025068b1f14630f9482114bd75cc1bc4ffede4a
Max Nesterov <braams@braams.ru>
2019-08-09 19:13:29 +0300

    Allow OIDMSLegacyKRB5 if the mechtype is OIDKRB5

--- spnego/negotiationToken.go
+++ spnego/negotiationToken.go
@@ -170,7 +170,7 @@ func (n *NegTokenInit) Verify() (bool, gssapi.Status) {
 		}
 	}
 	// RFC4178 states that the initial negotiation message can optionally contain the initial mechanism token for the preferred mechanism of the client.
-	if !mt.OID.Equal(n.MechTypes[0]) {
+	if !(mt.OID.Equal(n.MechTypes[0]) || (mt.OID.Equal(gssapi.OID(gssapi.OIDKRB5)) && gssapi.OID(gssapi.OIDMSLegacyKRB5).Equal(n.MechTypes[0]))) {
 		return false, gssapi.Status{Code: gssapi.StatusDefectiveToken, Message: "OID of MechToken does not match the first in the MechTypeList"}
 	}
 	// Verify the mechtoken
